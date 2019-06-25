5d4e80bce8cf60c3d52845235dbc3e8fd4866337
Evgeniy Abramov <kio@rocketguys.com>
2019-06-25 18:26:40 +0300

    chore // up version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Protobuf.MixProject do
   def project do
     [
       app: :exproto,
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
